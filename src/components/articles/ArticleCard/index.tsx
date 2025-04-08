a8b6c2101e3cfcd295a64f1216f222fe9d5f0d44
dewaleolaoye <adewaleolaoye21@gmail.com>
2025-04-08 11:56:40 +0100

    chore: initial commit

---
+++ src/components/articles/ArticleCard/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const ArticleCard = () => {
+  return (
+    <Box>
+      <Box>ArticleCard</Box>
+    </Box>
+  );
+};
+
+export default ArticleCard;
---
+++ src/components/articles/ArtilceList/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const ArtilcleList = () => {
+  return (
+    <Box>
+      <Box>ArtilcleList</Box>
+    </Box>
+  );
+};
+
+export default ArtilcleList;
